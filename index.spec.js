3220b3a7b4ffdc3d757c18cb7ba8d854beb43722
Cai Martin <cmartin.professional@yahoo.com>
2019-07-25 17:02:18 -0400

    added tests

--- index.spec.js
+++ index.spec.js
@@ -20,6 +20,7 @@ describe("GET /", () => {
       });
   });
 });
+/////////////////////////////////////////////////////
 describe("GET /:id", () => {
   it("should return 200 OK if users in DB", () => {
     return request(app)
@@ -39,3 +40,56 @@ describe("GET /:id", () => {
       });
   });
 });
+////////////////////////////////////////////////////////
+describe("GET /hello/:id", () => {
+  it("should return 200 OK if users in DB", () => {
+    return request(app)
+      .get("/hello/0")
+      .then(res => {
+        expect(res.status).toBe(200);
+      });
+  });
+});
+describe("GET /hello/:id", () => {
+  it("should return JSON DATA", () => {
+    return request(app)
+      .get("/hello/0")
+      .then(res => {
+        expect(res.type).toMatch(/json/);
+        expect(res.type).toBe("application/json");
+      });
+  });
+});
+////////////////////////////////////////////////////////
+describe("POST /", () => {
+  it("should return 201 OK if user can be added to database", () => {
+    return request(app)
+      .post("/")
+      .send({ name: "bob", age: 12 })
+      .then(res => {
+        expect(res.status).toBe(201);
+      });
+  });
+});
+describe("POST /", () => {
+  it("should return JSON DATA", () => {
+    return request(app)
+      .post("/")
+      .send({ name: "bob", age: 12 })
+      .then(res => {
+        expect(res.type).toMatch(/json/);
+        expect(res.type).toBe("application/json");
+      });
+  });
+});
+////////////////////////////////////////////////////////
+describe("DELETE /:id", () => {
+  it("should return 204 OK if user is in DB", () => {
+    return request(app)
+      .delete("/0")
+      .then(res => {
+        expect(res.status).toBe(204);
+      });
+  });
+});
+////////////////////////////////////////////////////////
